0d4479d17fc85e896b9dbc19e7c9962b9e2d5463
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-12-15 09:41:37 +0100

    Update to new hapi style

--- test/index.js
+++ test/index.js
@@ -1,58 +1,60 @@
-var Lab = require('lab');
-var Code = require('code');
-var Hapi = require('hapi');
-var Boom = require('boom');
-var lab = exports.lab = Lab.script();
+'use strict';
 
-var expect = Code.expect;
-var before = lab.before;
-var after = lab.after;
-var describe = lab.describe;
-var it = lab.it;
+const Lab = require('lab');
+const Code = require('code');
+const Hapi = require('hapi');
+const Boom = require('boom');
+const lab = exports.lab = Lab.script();
+
+const expect = Code.expect;
+const before = lab.before;
+const after = lab.after;
+const describe = lab.describe;
+const it = lab.it;
 
 
-var defaultHandler = function (request, reply) {
+const defaultHandler = (request, reply) => {
 
     reply('success');
 };
 
 
-var defaultValidateFunc = function (token, callback) {
+const defaultValidateFunc = (token, callback) => {
 
     return callback(null, token === '12345678',  { token: token });
 };
 
 
-var alwaysRejectValidateFunc = function (token, callback) {
+const alwaysRejectValidateFunc = (token, callback) => {
 
     return callback(null, false, { token: token });
 };
 
 
-var alwaysErrorValidateFunc = function (token, callback) {
+const alwaysErrorValidateFunc = (token, callback) => {
 
     return callback({ Error:'Error' }, false, null);
 };
 
 
-var boomErrorValidateFunc = function (token, callback) {
+const boomErrorValidateFunc = (token, callback) => {
 
     return callback(Boom.badImplementation('test info'), false, null);
 };
 
 
-var noCredentialValidateFunc = function (token, callback) {
+const noCredentialValidateFunc = (token, callback) => {
 
     return callback(null, true, null);
 };
 
-var server = new Hapi.Server({ debug: false });
+let server = new Hapi.Server({ debug: false });
 server.connection();
 
 
-before(function (done){
+before((done) => {
 
-    server.register(require('../'), function (err) {
+    server.register(require('../'), (err) => {
 
         expect(err).to.not.exist();
 
@@ -120,31 +122,18 @@ before(function (done){
 });
 
 
-after(function (done) {
+after((done) => {
 
     server = null;
     done();
 });
 
 
-it('returns 200 and success with correct bearer token header set', function (done) {
+it('returns 200 and success with correct bearer token header set', (done) => {
 
-    var request = { method: 'POST', url: '/basic', headers: { authorization: 'Bearer 12345678' } };
+    const request = { method: 'POST', url: '/basic', headers: { authorization: 'Bearer 12345678' } };
 
-    server.inject(request, function (res) {
-
-        expect(res.statusCode).to.equal(200);
-        expect(res.result).to.equal('success');
-        done();
-    });
-});
-
-
-it('returns 200 and success with correct bearer token header set in multiple authorization header', function (done) {
-
-    var request = { method: 'GET', url: '/multiple_headers_enabled', headers: { authorization: 'Bearer 12345678; FD AF6C74D1-BBB2-4171-8EE3-7BE9356EB018' } };
-
-    server.inject(request, function (res) {
+    server.inject(request, (res) => {
 
         expect(res.statusCode).to.equal(200);
         expect(res.result).to.equal('success');
@@ -153,24 +142,11 @@ it('returns 200 and success with correct bearer token header set in multiple aut
 });
 
 
-it('returns 200 and success with correct bearer token header set in multiple places of the authorization header', function (done) {
+it('returns 200 and success with correct bearer token header set in multiple authorization header', (done) => {
 
-    var request = { method: 'GET', url: '/multiple_headers_enabled', headers: { authorization: 'FD AF6C74D1-BBB2-4171-8EE3-7BE9356EB018; Bearer 12345678' } };
+    const request = { method: 'GET', url: '/multiple_headers_enabled', headers: { authorization: 'Bearer 12345678; FD AF6C74D1-BBB2-4171-8EE3-7BE9356EB018' } };
 
-    server.inject(request, function (res) {
-
-        expect(res.statusCode).to.equal(200);
-        expect(res.result).to.equal('success');
-        done();
-    });
-});
-
-
-it('returns 200 and success with correct bearer token query param set', function (done) {
-
-    var request = { method: 'POST', url: '/basic?access_token=12345678' };
-
-    server.inject(request, function (res) {
+    server.inject(request, (res) => {
 
         expect(res.statusCode).to.equal(200);
         expect(res.result).to.equal('success');
@@ -179,11 +155,37 @@ it('returns 200 and success with correct bearer token query param set', function
 });
 
 
-it('returns 401 error when no bearer token is set when one is required by default', function (done) {
+it('returns 200 and success with correct bearer token header set in multiple places of the authorization header', (done) => {
 
-    var request = { method: 'POST', url: '/basic_default_auth' };
+    const request = { method: 'GET', url: '/multiple_headers_enabled', headers: { authorization: 'FD AF6C74D1-BBB2-4171-8EE3-7BE9356EB018; Bearer 12345678' } };
 
-    server.inject(request, function (res) {
+    server.inject(request, (res) => {
+
+        expect(res.statusCode).to.equal(200);
+        expect(res.result).to.equal('success');
+        done();
+    });
+});
+
+
+it('returns 200 and success with correct bearer token query param set', (done) => {
+
+    const request = { method: 'POST', url: '/basic?access_token=12345678' };
+
+    server.inject(request, (res) => {
+
+        expect(res.statusCode).to.equal(200);
+        expect(res.result).to.equal('success');
+        done();
+    });
+});
+
+
+it('returns 401 error when no bearer token is set when one is required by default', (done) => {
+
+    const request = { method: 'POST', url: '/basic_default_auth' };
+
+    server.inject(request, (res) => {
 
         expect(res.statusCode).to.equal(401);
         done();
@@ -191,23 +193,11 @@ it('returns 401 error when no bearer token is set when one is required by defaul
 });
 
 
-it('returns 401 when bearer authorization header is not set', function (done) {
+it('returns 401 when bearer authorization header is not set', (done) => {
 
-    var request = { method: 'POST', url: '/basic', headers: { authorization: 'definitelynotacorrecttoken' } };
+    const request = { method: 'POST', url: '/basic', headers: { authorization: 'definitelynotacorrecttoken' } };
 
-    server.inject(request, function (res) {
-
-        expect(res.statusCode).to.equal(401);
-        done();
-    });
-});
-
-
-it('returns 401 error with bearer token type of object (invalid token)', function (done) {
-
-    var request = { method: 'POST', url: '/basic', headers: { authorization: 'Bearer {test: 1}' } };
-
-    server.inject(request, function (res) {
+    server.inject(request, (res) => {
 
         expect(res.statusCode).to.equal(401);
         done();
@@ -215,10 +205,22 @@ it('returns 401 error with bearer token type of object (invalid token)', functio
 });
 
 
-it('returns 500 when strategy returns a regular object to validateFunc', function (done) {
+it('returns 401 error with bearer token type of object (invalid token)', (done) => {
 
-    var request = { method: 'GET', url: '/basic_validate_error', headers: { authorization: 'Bearer 12345678' } };
-    server.inject(request, function (res) {
+    const request = { method: 'POST', url: '/basic', headers: { authorization: 'Bearer {test: 1}' } };
+
+    server.inject(request, (res) => {
+
+        expect(res.statusCode).to.equal(401);
+        done();
+    });
+});
+
+
+it('returns 500 when strategy returns a regular object to validateFunc', (done) => {
+
+    const request = { method: 'GET', url: '/basic_validate_error', headers: { authorization: 'Bearer 12345678' } };
+    server.inject(request, (res) => {
 
         expect(res.statusCode).to.equal(200);
         expect(JSON.stringify(res.result)).to.equal('{\"Error\":\"Error\"}');
@@ -227,10 +229,10 @@ it('returns 500 when strategy returns a regular object to validateFunc', functio
 });
 
 
-it('returns 500 when strategy returns a Boom error to validateFunc', function (done) {
+it('returns 500 when strategy returns a Boom error to validateFunc', (done) => {
 
-    var request = { method: 'GET', url: '/boom_validate_error', headers: { authorization: 'Bearer 12345678' } };
-    server.inject(request, function (res) {
+    const request = { method: 'GET', url: '/boom_validate_error', headers: { authorization: 'Bearer 12345678' } };
+    server.inject(request, (res) => {
 
         expect(res.statusCode).to.equal(500);
         expect(JSON.stringify(res.result)).to.equal('{\"statusCode\":500,\"error\":\"Internal Server Error\",\"message\":\"An internal server error occurred\"}');
@@ -239,10 +241,10 @@ it('returns 500 when strategy returns a Boom error to validateFunc', function (d
 });
 
 
-it('returns 401 handles when isValid false passed to validateFunc', function (done) {
+it('returns 401 handles when isValid false passed to validateFunc', (done) => {
 
-    var request = { method: 'GET', url: '/always_reject', headers: { authorization: 'Bearer 12345678' } };
-    server.inject(request, function (res) {
+    const request = { method: 'GET', url: '/always_reject', headers: { authorization: 'Bearer 12345678' } };
+    server.inject(request, (res) => {
 
         expect(res.statusCode).to.equal(401);
         done();
@@ -250,10 +252,10 @@ it('returns 401 handles when isValid false passed to validateFunc', function (do
 });
 
 
-it('returns 500 when no credentials passed to validateFunc', function (done) {
+it('returns 500 when no credentials passed to validateFunc', (done) => {
 
-    var request = { method: 'GET', url: '/no_credentials', headers: { authorization: 'Bearer 12345678' } };
-    server.inject(request, function (res) {
+    const request = { method: 'GET', url: '/no_credentials', headers: { authorization: 'Bearer 12345678' } };
+    server.inject(request, (res) => {
 
         expect(res.statusCode).to.equal(500);
         done();
@@ -261,22 +263,10 @@ it('returns 500 when no credentials passed to validateFunc', function (done) {
 });
 
 
-it('returns a 200 on successful auth with access_token query param renamed and set', function (done) {
+it('returns a 200 on successful auth with access_token query param renamed and set', (done) => {
 
-    var requestQueryToken = { method: 'GET', url: '/basic_named_token?my_access_token=12345678' };
-    server.inject(requestQueryToken, function (res) {
-
-        expect(res.statusCode).to.equal(200);
-        expect(res.result).to.equal('success');
-        done();
-    });
-});
-
-
-it('doesn\'t affect header auth and will return 200 and success when specifying custom access_token name', function (done) {
-
-    var requestQueryToken = { method: 'GET', url: '/basic_named_token', headers: { authorization: 'Bearer 12345678' } };
-    server.inject(requestQueryToken, function (res) {
+    const requestQueryToken = { method: 'GET', url: '/basic_named_token?my_access_token=12345678' };
+    server.inject(requestQueryToken, (res) => {
 
         expect(res.statusCode).to.equal(200);
         expect(res.result).to.equal('success');
@@ -285,10 +275,10 @@ it('doesn\'t affect header auth and will return 200 and success when specifying 
 });
 
 
-it('allows you to enable auth by query token', function (done) {
+it('doesn\'t affect header auth and will return 200 and success when specifying custom access_token name', (done) => {
 
-    var requestQueryToken = { method: 'GET', url: '/query_token_enabled?access_token=12345678' };
-    server.inject(requestQueryToken, function (res) {
+    const requestQueryToken = { method: 'GET', url: '/basic_named_token', headers: { authorization: 'Bearer 12345678' } };
+    server.inject(requestQueryToken, (res) => {
 
         expect(res.statusCode).to.equal(200);
         expect(res.result).to.equal('success');
@@ -297,10 +287,22 @@ it('allows you to enable auth by query token', function (done) {
 });
 
 
-it('allows you to disable auth by query token', function (done) {
+it('allows you to enable auth by query token', (done) => {
 
-    var requestHeaderToken  = { method: 'GET', url: '/query_token_disabled?access_token=12345678' };
-    server.inject(requestHeaderToken, function (res) {
+    const requestQueryToken = { method: 'GET', url: '/query_token_enabled?access_token=12345678' };
+    server.inject(requestQueryToken, (res) => {
+
+        expect(res.statusCode).to.equal(200);
+        expect(res.result).to.equal('success');
+        done();
+    });
+});
+
+
+it('allows you to disable auth by query token', (done) => {
+
+    const requestHeaderToken  = { method: 'GET', url: '/query_token_disabled?access_token=12345678' };
+    server.inject(requestHeaderToken, (res) => {
 
         expect(res.statusCode).to.equal(401);
         done();
@@ -308,20 +310,20 @@ it('allows you to disable auth by query token', function (done) {
 });
 
 
-it('disables multiple auth headers by default', function (done) {
+it('disables multiple auth headers by default', (done) => {
 
-    var request = { method: 'POST', url: '/basic', headers: { authorization: 'RandomAuthHeader 1234; Bearer 12345678' } };
-    server.inject(request, function (res) {
+    const request = { method: 'POST', url: '/basic', headers: { authorization: 'RandomAuthHeader 1234; Bearer 12345678' } };
+    server.inject(request, (res) => {
 
         expect(res.statusCode).to.equal(401);
         done();
     });
 });
 
-it('allows you to enable multiple auth headers', function (done) {
+it('allows you to enable multiple auth headers', (done) => {
 
-    var requestHeaderToken = { method: 'GET', url: '/multiple_headers_enabled', headers: { authorization: 'RandomAuthHeader 1234; Bearer 12345678' } };
-    server.inject(requestHeaderToken, function (res) {
+    const requestHeaderToken = { method: 'GET', url: '/multiple_headers_enabled', headers: { authorization: 'RandomAuthHeader 1234; Bearer 12345678' } };
+    server.inject(requestHeaderToken, (res) => {
 
         expect(res.statusCode).to.equal(200);
         done();
@@ -329,21 +331,10 @@ it('allows you to enable multiple auth headers', function (done) {
 });
 
 
-it('return unauthorized when no auth info and multiple headers disabled', function (done) {
+it('return unauthorized when no auth info and multiple headers disabled', (done) => {
 
-    var requestHeaderToken = { method: 'POST', url: '/basic', headers: { authorization: 'x' } };
-    server.inject(requestHeaderToken, function (res) {
-
-        expect(res.statusCode).to.equal(401);
-        done();
-    });
-});
-
-
-it('return unauthorized when no auth info and multiple headers enabled', function (done) {
-
-    var requestHeaderToken = { method: 'GET', url: '/multiple_headers_enabled', headers: { authorization: 'x' } };
-    server.inject(requestHeaderToken, function (res) {
+    const requestHeaderToken = { method: 'POST', url: '/basic', headers: { authorization: 'x' } };
+    server.inject(requestHeaderToken, (res) => {
 
         expect(res.statusCode).to.equal(401);
         done();
@@ -351,11 +342,10 @@ it('return unauthorized when no auth info and multiple headers enabled', functio
 });
 
 
-it('return unauthorized when different token type is used', function (done) {
+it('return unauthorized when no auth info and multiple headers enabled', (done) => {
 
-    var requestHeaderToken = { method: 'GET', url: '/custom_token_type', headers: { authorization: 'Bearer 12345678' } };
-
-    server.inject(requestHeaderToken, function (res) {
+    const requestHeaderToken = { method: 'GET', url: '/multiple_headers_enabled', headers: { authorization: 'x' } };
+    server.inject(requestHeaderToken, (res) => {
 
         expect(res.statusCode).to.equal(401);
         done();
@@ -363,11 +353,23 @@ it('return unauthorized when different token type is used', function (done) {
 });
 
 
-it('return 200 when correct token type is used', function (done) {
+it('return unauthorized when different token type is used', (done) => {
 
-    var requestHeaderToken  = { method: 'GET', url: '/custom_token_type', headers: { authorization: 'Basic 12345678' } };
+    const requestHeaderToken = { method: 'GET', url: '/custom_token_type', headers: { authorization: 'Bearer 12345678' } };
 
-    server.inject(requestHeaderToken, function (res) {
+    server.inject(requestHeaderToken, (res) => {
+
+        expect(res.statusCode).to.equal(401);
+        done();
+    });
+});
+
+
+it('return 200 when correct token type is used', (done) => {
+
+    const requestHeaderToken  = { method: 'GET', url: '/custom_token_type', headers: { authorization: 'Basic 12345678' } };
+
+    server.inject(requestHeaderToken, (res) => {
 
         expect(res.statusCode).to.equal(200);
         done();
